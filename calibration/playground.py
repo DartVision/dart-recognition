90bd3dfcd1930b619771e5f533e5878025b5d58f
Michael Markl <marklmichael98@gmail.com>
2020-09-24 02:05:33 +0200

    Calculate relative intersections

--- calibration/playground.py
+++ calibration/playground.py
@@ -20,29 +20,67 @@ def extract_mask(image, color):
 
 
 def mask_to_image(mask):
-    mask = np.tile(np.asarray(mask, dtype=np.int)[:, :, np.newaxis], reps=(1,1,3))
+    mask = np.tile(np.asarray(mask, dtype=np.int)[:, :, np.newaxis], reps=(1, 1, 3))
     image = np.ones_like(mask) * mask * 255
-    return image
+    return cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+
+
+def process_mask(mask):
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3)))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((5, 5), dtype=np.uint8))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((10, 10)))
+    return mask
+
+
+def fit_ideal_board_to_detection(ideal_points, detected_points):
+    a, b, phi = 0, 0, 0
+
+    return a, b, phi
 
 
 if __name__ == '__main__':
     image = cv2.imread('resources/board1.jpg', cv2.IMREAD_COLOR)
+
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+    # image[:, :, 0] = cv2.equalizeHist(image[:, :, 0])
+    # image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR)
+
     image = cv2.resize(image, (960, 720))
-    #  image = cv2.blur(image, (3, 3))
-    image = reduce_color_depth(image, 2)
+    image = cv2.blur(image, (3, 3))
+    # image = reduce_color_depth(image, 2)
     # cv2.imshow('as', image)
     # cv2.waitKey(0)
 
-    red_mask = extract_mask(image, (63, 63, 191))
-    green_mask = extract_mask(image, (63, 191, 63))
-    result = mask_to_image(red_mask)
-    result = mask_to_image(green_mask)
+    # compute masks and calculate intersections
+    # red_mask = extract_mask(image, (63, 63, 191))
+    # green_mask = extract_mask(image, (63, 191, 63))
+    # white_mask = extract_mask(image, (191, 191, 191))
+    # black_mask = extract_mask(image, (63, 63, 63))
+    # red_mask = mask_to_image(red_mask)
+    # green_mask = mask_to_image(green_mask)
+    # white_mask = mask_to_image(white_mask)
+    # black_mask = mask_to_image(black_mask)
+    # red_mask = process_mask(red_mask)
+    # green_mask = process_mask(green_mask)
+    # white_mask = process_mask(white_mask)
+    # black_mask = process_mask(black_mask)
+    # edges_gr = cv2.bitwise_and(red_mask, green_mask)
+    # edges_bw = cv2.bitwise_and(white_mask, black_mask)
+    # edges = cv2.bitwise_and(edges_bw, edges_gr)
+    #
+    # result = edges
+    #
+    # green_mask = cv2.resize(green_mask, (1920, 1080))
+    # cv2.imshow('asdf', green_mask)
+    # cv2.waitKey(0)
+
+    # result = mask_to_image(green_mask)
     # pixel_colors = image.reshape((np.shape(image)[0] * np.shape(image)[1], 3))
     # norm = colors.Normalize(vmin=-1., vmax=1.)
     # norm.autoscale(pixel_colors)
     # pixel_colors = norm(pixel_colors).tolist()
 
-    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    # hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
     # h, s, v = cv2.split(hsv_image)
     # fig = plt.figure()
     # axis = fig.add_subplot(1, 1, 1, projection="3d")
@@ -56,28 +94,24 @@ if __name__ == '__main__':
     edges = cv2.Canny(image, 50, 150, apertureSize=3)
     lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
 
+    result = edges
+
     for line in lines:
         rho, theta = line[0]
         a = np.cos(theta)
         b = np.sin(theta)
         x0 = a * rho
         y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
+        x1 = int(x0 + 10000 * (-b))
+        y1 = int(y0 + 10000 * (a))
+        x2 = int(x0 - 10000 * (-b))
+        y2 = int(y0 - 10000 * (a))
 
         cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-    # red-green segmentation
-    red1 = (1, 190, 200)
-    red2 = (18, 255, 255)
-    green1 = (30, 100, 50)
-    green2 = (75, 255, 255)
 
     image = image[:, :, [2, 1, 0]]
     plt.subplot(121)
     plt.imshow(image)
     plt.subplot(122)
-    plt.imshow(result)
+    plt.imshow(result, cmap='gray')
     plt.show()
--- calibration/playground2.py
+++ calibration/playground2.py
@@ -1,62 +1,88 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from itertools import combinations
 from matplotlib import colors
-
-MAX_FEATURES = 500
-MATCH_THRESHOLD = 0.15
+from sklearn.cluster import DBSCAN
 
 
-def align_images(base_image, image_to_align):
-    image1 = cv2.cvtColor(base_image, cv2.COLOR_BGR2GRAY)
-    image2 = cv2.cvtColor(image_to_align, cv2.COLOR_BGR2GRAY)
-
-    orb = cv2.ORB_create(MAX_FEATURES)
-    keypoints1, desc1 = orb.detectAndCompute(image1, None)
-    keypoints2, desc2 = orb.detectAndCompute(image2, None)
-
-    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-    matches = matcher.match(desc1, desc2, None)
-
-    matches.sort(key=lambda x: x.distance, reverse=False)
-
-    # Remove not bad matches
-    num_good_matches = int(len(matches) * MATCH_THRESHOLD)
-    matches = matches[:num_good_matches]
-
-    # Draw top matches
-    matches_on_image = cv2.drawMatches(image1, keypoints1, image2, keypoints2, matches, None)
-    cv2.imshow("matches", matches_on_image)
+def f(image):
+    pass
+    lines = detect_lines(image)
+    center = calculate_center(lines)
+    for inters in center:
+        cv2.circle(image, tuple((np.asarray(inters) * (image.shape[:2][::-1])).astype(np.int)), 3, (0, 255, 0), thickness=3)
+    image = cv2.resize(image, (960, 720))
+    cv2.imshow('as', image)
     cv2.waitKey(0)
 
-    # Extract location of good matches
-    points1 = np.zeros((len(matches), 2), dtype=np.float32)
-    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+    # green_areas = detect_green_areas(image)
+    # green_areas = discard_inner_ellipsoids(green_areas, center)
+    # corners = calculate_corners(image) # not clear yet how to do this reliably
+    # a, b = select_opposing_starting_points(corners, center)
+    # c, d = detect_perpendicular_points(green_areas, corners)
+    # return a, b, c, d
 
-    for i, match in enumerate(matches):
-        points1[i, :] = keypoints1[match.queryIdx].pt
-        points2[i, :] = keypoints2[match.trainIdx].pt
 
-    # Find homography
-    homography, mask = cv2.findHomography(points2, points1, cv2.RANSAC)
+def detect_lines(image):
+    image = cv2.resize(image, (960, 720))
+    image = cv2.blur(image, (3, 3))
+    edges = cv2.Canny(image, 50, 150, apertureSize=3)
+    lines = cv2.HoughLines(edges, rho=1, theta=np.pi / 180, threshold=200)
 
-    # Use homography
-    height, width, channels = base_image.shape
-    image2_aligned = cv2.warpPerspective(image_to_align, homography, (width, height))
+    rhos = lines[:, 0, 0]
+    thetas = lines[:, 0, 1]
+    x = rhos * np.cos(thetas)
+    y = rhos * np.sin(thetas)
+    h, w = image.shape[:2]
 
-    return image2_aligned, homography
+    t = (x * y * (w*w - h*h)) / (y*y*h*h + x*x*w*w)
+    F1 = x / w + t * y / w
+    F2 = y / h - t * x / h
+
+    rhos = np.linalg.norm([F1, F2], axis=0)
+    thetas = np.arctan2(F2, F1)
+
+    return np.stack([rhos, thetas], axis=-1)
+
+
+def calculate_intersections(lines):
+    intersections = []
+    for line1, line2 in combinations(lines, 2):
+        rho1, theta1 = line1
+        rho2, theta2 = line2
+        denominator = np.sin(theta1) * np.cos(theta2) - np.sin(theta2) * np.cos(theta1)
+        if np.abs(denominator) < 0.001:
+            # lines are (almost) parallel
+            continue
+        else:
+            y = (rho1 * np.cos(theta2) - rho2 * np.cos(theta1)) / denominator
+            x = (rho1 - y * np.sin(theta1)) / np.cos(theta1)
+            intersections.append((x, y))
+
+    return intersections
+
+
+def calculate_center(lines):
+    pass
+    # option 1:
+    # center_estimate = calculate_nearest_point(lines)
+    # lines_closest_to_estimate = select_closest_lines(lines, center_estimate, percentile)
+    # return calculate_closest_point(lines_closest_to_estimate)
+
+    # option 2:
+    intersections = calculate_intersections(lines)
+    return intersections
+    # clusters = DBSCAN(eps=10, min_samples=3).fit(intersections)
+    # return clusters
+    # return np.average(filtered_intersections, axis=0)
 
 
 if __name__ == '__main__':
     image = cv2.imread('resources/board1.jpg', cv2.IMREAD_COLOR)
-    image2 = cv2.imread('resources/board2.jpg', cv2.IMREAD_COLOR)
-    image = cv2.resize(image, (960, 720))
-    image2 = cv2.resize(image2, (960, 720))
 
-    aligned, homography = align_images(image, image2)
+    f(image)
 
-    #  image = cv2.blur(image, (3, 3))
-    # image = reduce_color_depth(image, 2)
-    aligned = cv2.resize(aligned, (960, 720))
-    cv2.imshow('as', aligned)
-    cv2.waitKey(0)
+    # aligned = cv2.resize(image, (960, 720))
+    # cv2.imshow('as', aligned)
+    # cv2.waitKey(0)
