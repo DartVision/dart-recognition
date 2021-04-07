ebb512c80a77096f4167a54d03377c57a19531d6
Nikolas Klug <klug.nikolas@googlemail.com>
2021-04-07 18:52:55 +0200

    Add dataset loading

--- datasets/darts/darts.py
+++ datasets/darts/darts.py
@@ -4,19 +4,20 @@ import tensorflow_datasets as tfds
 import tensorflow as tf
 from os import path
 import json
-from enum import Enum
 
 _DESCRIPTION = """
-Description is **formatted** as markdown.
+**Darts dataset**
 
-It should also contain any processing which has been applied (if any),
-(e.g. corrupted example skipped, images cropped,...):
+Contains images of darts on a dartboard with the following annotations:
+- entry points of darts into the dartboard
+- color of field darts entered
+
 """
 
 PATH = '~/datasets/darts/data'
 
 
-class COLORS(Enum):
+class COLORS(object):
     BLACK = 0
     WHITE = 1
     RED = 2
@@ -24,14 +25,14 @@ class COLORS(Enum):
 
 
 SCORE_COLORS = {}
-_color_scores = [{str(i): COLORS.BLACK for i in [20, 18, 13, 10, 2, 3, 7, 8, 14, 12]},
+_color_scores = [{str(i): COLORS.BLACK for i in [0, 20, 18, 13, 10, 2, 3, 7, 8, 14, 12]},
                  {str(i): COLORS.WHITE for i in [1, 4, 6, 15, 17, 19, 16, 11, 9, 5]},
-                 {str(i): COLORS.RED for i in
+                 {str(i): COLORS.GREEN for i in
                   ['D1', 'T1', 'D4', 'T4', 'D6', 'T6', 'D15', 'T15', 'D17', 'T17', 'D19', 'T19',
-                   'D11', 'T11', 'D9', 'T9', 'D5', 'T5', '25']},
-                 {str(i): COLORS.BLACK for i in
+                   'D16', 'T16', 'D11', 'T11', 'D9', 'T9', 'D5', 'T5', '25']},
+                 {str(i): COLORS.RED for i in
                   ['D20', 'T20', 'D18', 'T18', 'D13', 'T13', 'D10', 'T10', 'D2', 'T2',
-                   'D7', 'T7', 'D14', 'T14', 'D12', 'T12', '50']}]
+                   'D7', 'T7', 'D8', 'T8', 'D14', 'T14', 'D12', 'T12', 'D25']}]
 
 for color_scores in _color_scores:
     SCORE_COLORS.update(color_scores)
@@ -74,23 +75,26 @@ class Darts(tfds.core.GeneratorBasedBuilder):
         """Yields examples."""
         with open(path.join(directory, f'{split}.txt'), 'r') as split_file:
             split_names = split_file.readlines()
+        split_names = [name.strip('\n') for name in split_names]
         for f in split_names:
             json_name = path.join(directory, f)
             with open(json_name, 'rb') as json_file:
                 annotation = json.load(json_file)
                 image_name = annotation['imageName']
+                image_path = path.join(directory, image_name)
                 w, h = annotation['imageWidth'], annotation['imageHeight']
                 labels = []
                 for shape in annotation['shapes']:
                     if shape['label'] == 'dart':
                         x, y = shape['point']
                         label = {
-                            'x': x / w,
-                            'y': y / h,
+                            # Convert to relative locations in [-1, 1]
+                            'x': (2*x - w) / w,
+                            'y': (2*y - h) / h,
                             'color': SCORE_COLORS[shape['score']]
                         }
                         labels.append(label)
                 yield f, {
-                    'image': image_name,
-                    'label': labels,
+                    'image': image_path,
+                    'labels': labels,
                 }
